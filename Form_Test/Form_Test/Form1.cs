112b3ad098d26a49741a80713fac9fbc773d5027
DESKTOP-8EI0TNC\be-ma <bemagicalmiyuki@gmail.com>
2025-10-24 15:36:56 +0900

    ボタンたくさん入れたやつ

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -13,8 +13,33 @@ namespace Form_Test
     public partial class Form1 : Form
     {
         public Form1()
+
         {
             InitializeComponent();
+            for (int i = 0; i < 6;  i++)
+                for (int j = 0; j < 4; j++)
+                {
+                
+                    TestButton testButton = new TestButton();
+
+                testButton.Location = new Point(50 * i, 50 * j);
+                testButton.Size = new Size(50, 50);
+                testButton.Text = ("(^▽^)/");
+                Controls.Add(testButton);
+            }
+        }
+
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ハズレで～す( ´∀｀ )");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ハズレで～す( ´∀｀ )");
         }
     }
 }
